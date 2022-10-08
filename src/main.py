7039a9bb56c9752d7f2b2ac39bf35f29a05a2cfd
Ben Scuron <bscuron19@gmail.com>
2022-10-08 12:08:16 -0400

    Add cosine similarity and length difference features

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import pandas as pd
+import numpy as np
 import ssl
 from nltk import download, word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 import string
 from Levenshtein import distance as levenshtein_distance
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 DATA_TRAIN_PATH='../data/train_with_label.txt'
 DATA_DEV_PATH='../data/dev_with_label.txt'
@@ -13,9 +16,9 @@ FEATURE_COLUMNS = ['LEVENSHTEIN_DIST']
 
 def main():
     data_train, data_dev, data_test = get_data()
-    print(data_train)
-    print(data_dev)
-    print(data_test)
+    print(data_train[['LEVENSHTEIN_DIST', 'COSINE_SIMILARITY', 'GROUND_TRUTH']])
+    print(data_dev[['LEVENSHTEIN_DIST', 'COSINE_SIMILARITY', 'GROUND_TRUTH']])
+    print(data_test[['LEVENSHTEIN_DIST', 'COSINE_SIMILARITY', 'GROUND_TRUTH']])
 
 # Read and clean the train set, dev set, and test set. Return each in a tuple in the order (train, dev, test)
 def get_data():
@@ -36,7 +39,30 @@ def clean(df):
 # TODO
 def extract_features(df):
     df['LEVENSHTEIN_DIST'] = get_levenshtein_distance(df)
+    df['LENGTH_DIFFERENCES'] = get_length_difference(df)
+    df['COSINE_SIMILARITY'] = get_cosine_similarity(df)
     return df
+
+def get_cosine_similarity(df):
+    vectorizer = TfidfVectorizer()
+    corpus = []
+    for s1, s2 in zip(df['SENTENCE_1'], df['SENTENCE_2']):
+        corpus.append(s1)
+        corpus.append(s2)
+    vectorizer.fit(corpus)
+    sentence_1_vectors = df['SENTENCE_1'].apply(lambda x: np.array([value for value in vectorizer.transform([x]).A]).flatten())
+    sentence_2_vectors = df['SENTENCE_2'].apply(lambda x: np.array([value for value in vectorizer.transform([x]).A]).flatten())
+    cosine_simililarity_vector = []
+    for vec1, vec2 in zip(sentence_1_vectors, sentence_2_vectors):
+        cosine_simililarity_vector.append(cosine_similarity(vec1.reshape(1, -1), vec2.reshape(1, -1))[0][0])
+    return cosine_simililarity_vector
+
+def get_length_difference(df):
+    differences = []
+    for s1, s2 in zip(df['SENTENCE_1'], df['SENTENCE_2']):
+        differences.append(abs(len(s1) - len(s2)))
+    return differences
+
 
 def get_levenshtein_distance(df):
     distances = []
