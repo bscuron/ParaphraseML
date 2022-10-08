6a7a730efe22df1323a6db950e21b89e4255c402
Ben Scuron <bscuron19@gmail.com>
2022-10-08 18:46:47 -0400

    Add fuzzy features (NOT USED)

--- src/main.py
+++ src/main.py
@@ -14,19 +14,20 @@ from sklearn.metrics import accuracy_score
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import Normalizer
 from sklearn.preprocessing import StandardScaler
+from thefuzz.fuzz import ratio, partial_ratio, token_sort_ratio, token_set_ratio
 
 DATA_TRAIN_PATH='../data/train_with_label.txt'
 DATA_DEV_PATH='../data/dev_with_label.txt'
 DATA_TEST_PATH='../data/test_without_label.txt'
-FEATURE_COLUMNS = ['LEVENSHTEIN_DIST', 'COSINE_SIMILARITY', 'LENGTH_DIFFERENCES', 'SHARED_WORDS']
+FEATURE_COLUMNS = ['LEVENSHTEIN_DIST', 'COSINE_SIMILARITY', 'LENGTH_DIFFERENCE', 'SHARED_WORDS']
 
 def main():
     print('Reading, cleaning, analyzing data...')
     data_train, data_dev, data_test = get_data()
     data_train.to_csv('./train.csv')
-    print(data_train[['LEVENSHTEIN_DIST', 'COSINE_SIMILARITY', 'SHARED_WORDS', 'GROUND_TRUTH']])
-    print(data_dev[['LEVENSHTEIN_DIST', 'COSINE_SIMILARITY', 'SHARED_WORDS', 'GROUND_TRUTH']])
-    print(data_test[['LEVENSHTEIN_DIST', 'COSINE_SIMILARITY', 'SHARED_WORDS']])
+    print(data_train[FEATURE_COLUMNS + ['GROUND_TRUTH']])
+    print(data_dev[FEATURE_COLUMNS + ['GROUND_TRUTH']])
+    print(data_test[FEATURE_COLUMNS])
 
     # print("Finding optimal hyperparameters...")
     # param_grid = {'C': [0.01, 0.1, 1, 10, 100, 1000, 2000], 'kernel': ['rbf', 'sigmoid']}
@@ -58,10 +59,38 @@ def clean(df):
 # TODO
 def extract_features(df):
     df['LEVENSHTEIN_DIST'] = get_levenshtein_distance(df)
-    df['LENGTH_DIFFERENCES'] = get_length_difference(df)
+    df['LENGTH_DIFFERENCE'] = get_length_difference(df)
     df['COSINE_SIMILARITY'] = get_cosine_similarity(df)
     df['SHARED_WORDS'] = get_shared_words(df)
+    # df['RATIO'] = get_ratios(df)
+    # df['PARTIAL_RATIO'] = get_partial_ratios(df)
+    # df['TOKEN_SORT_RATIO'] = get_token_sort_ratios(df)
+    # df['TOKEN_SET_RATIO'] = get_token_set_ratios(df)
     return df
+
+def get_ratios(df):
+    ratios = []
+    for s1, s2 in zip(df['SENTENCE_1'], df['SENTENCE_2']):
+        ratios.append(ratio(s1, s2))
+    return ratios
+
+def get_partial_ratios(df):
+    ratios = []
+    for s1, s2 in zip(df['SENTENCE_1'], df['SENTENCE_2']):
+        ratios.append(partial_ratio(s1, s2))
+    return ratios
+
+def get_token_sort_ratios(df):
+    ratios = []
+    for s1, s2 in zip(df['SENTENCE_1'], df['SENTENCE_2']):
+        ratios.append(token_sort_ratio(s1, s2))
+    return ratios
+
+def get_token_set_ratios(df):
+    ratios = []
+    for s1, s2 in zip(df['SENTENCE_1'], df['SENTENCE_2']):
+        ratios.append(token_set_ratio(s1, s2))
+    return ratios
 
 def get_shared_words(df):
     shared_words_list = []
